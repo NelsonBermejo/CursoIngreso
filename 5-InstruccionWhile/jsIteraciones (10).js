0319d8fc76627950b9499c46dfd286541a406aca
Nelson Bermejo <nelsonbermejo@outlook.com.ar>
2017-07-14 19:52:08 -0300

    9 Y DIEZ

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,10 +1,14 @@
 function Mostrar()
 {
-	var respuesta="si",num=0,neg=0;contneg=0,pos=0,contpos=0,ceros=0,pares=0,promneg=0,prompos=0,dif=0;
+	var respuesta="si",num=0,neg=0;contneg=0,pos=0,contpos=0,ceros=0,pares=0,promneg=0,prompos=0,dif=0;//diferencia siempre es positiva
 
 	while(respuesta=="si")
 	{
 		num=parseInt(prompt("Ingrese un numero:"));
+		while (isNaN(num));//esto agreggue
+		{
+                   num=prompt("Ingrese un numero");
+		}
 		if (num<0)
 		{
 			neg=neg+num;
@@ -23,11 +27,38 @@ function Mostrar()
 		{
 			pares++;
 		}
-		respuesta=prompt("Desea ingresar otro numero?");
+		respuesta=prompt("Desea ingresar otro numero?");//rta=confirm();
 	}
-	promneg=neg/contneg;
-	prompos=pos/contpos;
-	dif=pos-neg;
+	promneg=neg/contneg;/*if(contpos==0)
+	                       {
+							   pp=("no ingreso numero");
+						   }         
+						   if(unNaN(pn))
+						   {
+							   pn=("")
+						   }
+						   */ 
+	prompos=pos/contpos;//si solamente ingresamos positivos, 0/0, cero dividido cero 
+	dif=pos-neg;        // diferencia de positivo menos negativos
+	                        /*if(contadorpos<contNeg)
+							{
+								dif=contneg-contpos;
+							}
+							else
+							{
+                                dif=contpos-contneg;
+							}
+							             OTRA FORMA;Ò
+
+							if (dif<0)
+							{
+                                 dif*=-1;
+							}
+							
+
+							
+							
+							*/ 
 	document.write("La suma de los negativos es: "+neg+"<br/>La suma de los positivos es: "+pos+
 					"<br/>La cantidad de negativos es: "+contneg+"<br/>La cantidad de positivos es: "+contpos+
 					"<br/>La cantidad de ceros es: "+ceros+"<br/>La cantidad de numeros pares es: "+pares+
@@ -35,3 +66,5 @@ function Mostrar()
 					"<br/>El promedio de los numeros negativos es: "+promneg+
 					"<br/>La diferencia entre positivos y negativos es: "+dif);
 }//FIN DE LA FUNCIÓN
+
+/
