07b99928953e7387b654223b4ee981accca2fb76
Nelson Bermejo <nelsonbermejo@outlook.com.ar>
2017-07-14 22:14:55 -0300

    revisar!!!

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -4,26 +4,86 @@ siendo 1 para “piedra”, el 2 para “papel” y 3 para “tijera”.
 El jugador seleccionará una imagen correspondiente 
 a su opción  y le informaremos si ganó, empató o perdió.
 */
+//eleccion maquina
+var resultado;
+var contEmpato=0;
+var contperdio=0;
+var contgano=0;
 var eleccionMaquina;
-
 function comenzar()
-{
-	
+{ eleccionMaquina;//eleccion maquina;
+	 eleccionMaquina=Math.floor(Math.random()*(4-1))+1;
+        console.log(eM);
+
 
 
 }//FIN DE LA FUNCIÓN
 function piedra()
-{
-	
-
+{   comenzar()
+	switch(eleccionMaquina)
+    {    case 1:
+        
+        resultado("Empato");//resultado=empato++
+           contEmpato++
+           Break;
+           case 2:
+           resultado="Pierde";
+           contperdio++;
+           Break;
+           case 3:
+           resultado="Gano";
+           contgano++;
+           Break;
+    }
+mostrar()
 }//FIN DE LA FUNCIÓN
 function papel()
 {
-
-
+  comenzar()	
+  switch(eleccionMaquina)
+    {    case 1:
+        
+        resultado="Empato";//resultado=empato++
+           contEmpato++
+           Break;
+           case 2:
+           resultado="Pierde";
+           contperdio++;
+           Break;
+           case 3:
+           resultado="Gano";
+           contgano++;
+           Break;
+    }
+mostrar()
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
+	 	switch(eleccionMaquina)
+    {    case 1:
+        
+        resultado("Empato");//resultado=empato++
+           contEmpato++
+           Break;
+           case 2:
+           resultado="Pierde";
+           contperdio++;
+           Break;
+           case 3:
+           resultado="Gano";
+           contgano++;
+           Break;
+        
+}
+mostrar()
+}
+//FIN DE LA FUNCIÓN
 
-}//FIN DE LA FUNCIÓN
+function mostrar()
+{      
+   
+     
+    alert (resultado+contgano+contperdio+contEmpato);
+      
+
+}
